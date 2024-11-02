2ee44b2fc3c410b583161d5814d130d64e39f17d
iceblaeze <69413504+iceblaeze@users.noreply.github.com>
2024-11-02 21:15:17 +0000

    Update stock_pouchdb.js

--- src/components/stock_pouchdb.js
+++ src/components/stock_pouchdb.js
@@ -1,5 +1,11 @@
 import {initialisedb} from './databases.js';
 const dbs = new PouchDB('stocks');
 
+export const getStocks = (id) => {
+  dbs.get(code)
+    .then(doc => console.log("Stock price:", doc.price))
+    .catch(error => console.error("Stock not found:", error));
+};
+
 // Call the function to initialize the inventory
 initialisedb('./stocks.json', dbs);

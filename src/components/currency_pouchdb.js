442d2831125fedbec8cb4a93f6546415bf68e2c1
iceblaeze <69413504+iceblaeze@users.noreply.github.com>
2024-11-02 21:01:02 +0000

    Update currency_pouchdb.js

--- src/components/currency_pouchdb.js
+++ src/components/currency_pouchdb.js
@@ -1,23 +1,5 @@
+import {initialisedb} from './databases.js';
 const dbc = new PouchDB('currencies');
 
-async function initialiseCurrency() {
-    const response = await fetch('./currencies.json');
-    const jsonData = await response.json();
-    const initialCurrenciesMORP = jsonData.map(stock => ({
-            ...stock,
-        price: Math.random()*500 // Add a new field 'active' with a default value
-    }));
-
-    // Bulk insert modified documents into PouchDB
-    try {
-        // Bulk insert initial items into the database
-        const response = await dbc.bulkDocs(initialCurrencies);
-        console.log('Currency database initialized successfully:', response);
-    } catch (error) {
-        console.error('Error initializing currencies:', error);
-    }
-};
-
-
 // Call the function to initialize the inventory
-initialiseCurrency();
+initialisedb('./currencies.json', dbc);

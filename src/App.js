8e4269d86c5660439175f3656d4acc55521e0527
iceblaeze <69413504+iceblaeze@users.noreply.github.com>
2024-11-02 16:16:20 +0000

    Update App.js
    added stock stuff

--- src/App.js
+++ src/App.js
@@ -5,15 +5,11 @@ import Sidebar from './components/Sidebar';
 import Portfolio from './components/Portfolio';
 import StockInput from './components/StockInput';
 import assetsData from './currencies.json';
+import {addStock, fetchStocks, updateStock, deleteStock, addCurrency, fetchCurrencies, updateCurrency, deleteCurrency} from './pouchdb.js';
 
 function App() {
-  const [portfolio, setPortfolio] = useState([]);
   
   const assets = assetsData;
-
-  const addStock = (ticker) => {
-    setPortfolio([...portfolio, { ticker, price: Math.random() * 1000 }]);
-  };
 
   return (
     <div style={styles.container}>

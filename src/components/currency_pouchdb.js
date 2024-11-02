c14897b1509265ff794d4020832a8f0d1a0c905c
iceblaeze <69413504+iceblaeze@users.noreply.github.com>
2024-11-02 21:12:44 +0000

    Update currency_pouchdb.js

--- src/components/currency_pouchdb.js
+++ src/components/currency_pouchdb.js
@@ -1,6 +1,12 @@
 import {initialisedb} from './databases.js';
 const dbc = new PouchDB('currencies');
 
+export const getCurrency = (id) => {
+  db.get(code)
+    .then(doc => console.log("Currency price:", doc.price))
+    .catch(error => console.error("Currency not found:", error));
+};
+
 // Call the function to initialize the inventory
 initialisedb('./currencies.json', dbc);
 dbc.put({

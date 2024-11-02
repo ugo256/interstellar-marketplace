8d5ce6003e05e0f391f1a99dbc99b46ddc826d9e
iceblaeze <69413504+iceblaeze@users.noreply.github.com>
2024-11-02 21:07:03 +0000

    Update currency_pouchdb.js

--- src/components/currency_pouchdb.js
+++ src/components/currency_pouchdb.js
@@ -3,3 +3,7 @@ const dbc = new PouchDB('currencies');
 
 // Call the function to initialize the inventory
 initialisedb('./currencies.json', dbc);
+dbc.put({
+  _id: 'UNI',
+  _rev: doc._rev,
+  price: 1.0})

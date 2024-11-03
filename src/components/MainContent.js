73ff1e34f6b403ad357c61f4a198d720401ab555
Ugo Obudulu <ug.obudulu@gmail.com>
2024-11-03 17:45:57 +0000

    added currency label

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -5,14 +5,16 @@ import { GetCurrency } from './utils';
 import { GetPlanet } from './utils';
 
 function MainContent({ stocks, currPlanet }) {
+    const curr = GetCurrency(currPlanet.currency);
       return (
     <div>
       <h1>{currPlanet.adj} Stock Prices</h1>
+      <h3>currency: {curr.name} ({curr.index})</h3>
       <ul>
         {stocks.map((stock) => (
           <li key={stock._id} hidden={currPlanet.name != stock.planet}>
             {stock.id}
-            <strong>{stock.index} - {stock.name}</strong>: {GetCurrency(currPlanet.currency).symbol}{stock.price.toFixed(2)}
+            <strong>{stock.index} - {stock.name}</strong>: {curr.symbol}{stock.price.toFixed(2)}
           </li>
         ))}
       </ul>

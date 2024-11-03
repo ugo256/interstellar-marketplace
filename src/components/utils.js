74628ac5f31b426ee2139bb29b4745f6f4c4308a
Ugo Obudulu <ug.obudulu@gmail.com>
2024-11-03 12:46:45 +0000

    added back currency symbols

--- src/components/utils.js
+++ src/components/utils.js
@@ -2,11 +2,11 @@ import planetsData from './planets.json';
 import currencyData from './currencies.json';
 
 export const getCurrency = (currName) => {
-    const currency = currencyData.find(c => c.index === currName);
+    const currency = currencyData.find(c => c.id === currName);
     return currency ? currency : 'Currency not found';
 };
 
 export const getPlanet = (planetName) => {
-    const currency = planetsData.find(c => c.index === planetName);
+    const currency = planetsData.find(c => c.id === planetName);
     return currency ? currency : 'Currency not found';
 };

27502a738deccf68063332cd2a9d758fe5055b0b
iceblaeze <69413504+iceblaeze@users.noreply.github.com>
2024-11-03 15:27:33 +0000

    Update MainContent.js

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import {stockPrice} from './stock_pouchdb.js';
+import dbs from './stock_pouchdb.js';
+
 
 function MainContent({ stocks }) {
-    return (
-        <div>
-            
+
+      return (
+    <div>
+      <h1>Stock Prices</h1>
+      <ul>
         {stocks.map((stock) => (
-            <li key={stock._id}>
-            <strong>{stock.name}</strong> (Price: {stock.price})
-            </li>
-            ))}
-        </div>
-    );
-  };
+          <li key={stock._id}>
+            {stock.id} 
+            <strong>{stock.name}</strong>: ${stock.price.toFixed(2)}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
   export default MainContent;
